7fe0d54fe85f3a8fcdb30358ada27bcd6a3112d4
francisco lobaton <30603805+fjasis@users.noreply.github.com>
2018-06-23 17:46:02 -0400

    actualizado

--- minimax.c
+++ minimax.c
@@ -3,71 +3,83 @@
 #include<math.h>
 #include"minimax_vieja_v1.c"
  
-struct Move
-{
-    int row, col;
+struct Movida{//Move
+    int fil, col;
 };
+int oponente;
  
-seleccioncaracter();
+char caracterus=seleccioncaracter();
+
+char caracteroponente(char caracterus){
+    if(caracterus==X){
+        oponente=O;
+    }
+    else if(caracterus==O){
+        oponente=X;
+    }
+}
 
 // This function returns true if there are moves
 // remaining on the board. It returns false if
 // there are no moves left to play.
-bool isMovesLeft(char board[3][3])
-{
+bool Siquedanmovimientos(char matriz[3][3]){//ismovesleft
     for (int i = 0; i<3; i++)
         for (int j = 0; j<3; j++)
-            if (board[i][j]=='_')
+            if (matriz[i][j]==' ')
                 return true;
     return false;
 }
  
 // This is the evaluation function as discussed
 // in the previous article ( http://goo.gl/sJgv68 )
-int evaluate(char b[3][3])
-{
+int evaluacion(char b[3][3],char caracterus,char oponente){//evaluate
+    int fil,col;
     // Checking for Rows for X or O victory.
-    for (int row = 0; row<3; row++)
+    for (fil = 0; fil<3; fil++)
     {
-        if (b[row][0]==b[row][1] &&
-            b[row][1]==b[row][2])
-        {
-            if (b[row][0]==player)
+        if (b[fil][0]==b[fil][1] &&b[fil][1]==b[fil][2]){
+            if (b[fil][0]==caracterus){
                 return +10;
-            else if (b[row][0]==opponent)
+            }
+            else if (b[fil][0]==oponente){
                 return -10;
+            }
         }
     }
  
     // Checking for Columns for X or O victory.
-    for (int col = 0; col<3; col++)
+    for (col = 0; col<3; col++)
     {
-        if (b[0][col]==b[1][col] &&
-            b[1][col]==b[2][col])
+        if (b[0][col]==b[1][col] &&b[1][col]==b[2][col])
         {
-            if (b[0][col]==player)
+            if (b[0][col]==caracterus){
                 return +10;
- 
-            else if (b[0][col]==opponent)
+            }
+            else if (b[0][col]==oponente){
                 return -10;
+            }
         }
     }
  
     // Checking for Diagonals for X or O victory.
     if (b[0][0]==b[1][1] && b[1][1]==b[2][2])
     {
-        if (b[0][0]==player)
+        if (b[0][0]==caracterus){
             return +10;
-        else if (b[0][0]==opponent)
+        }
+        else if (b[0][0]==oponente){
             return -10;
+        }
     }
  
     if (b[0][2]==b[1][1] && b[1][1]==b[2][0])
     {
-        if (b[0][2]==player)
+        if (b[0][2]==caracterus){
             return +10;
-        else if (b[0][2]==opponent)
+        }
+        else if (b[0][2]==oponente){
             return -10;
+        }
     }
  
     // Else if none of them have won then return 0
@@ -77,129 +89,124 @@ int evaluate(char b[3][3])
 // This is the minimax function. It considers all
 // the possible ways the game can go and returns
 // the value of the board
-int minimax(char board[3][3], int depth, bool isMax)
+int minimax(char matriz[3][3], int depth, bool isMax)
 {
-    int score = evaluate(board);
+    int puntaje = evaluacion(matriz);
+    int i,j;
  
     // If Maximizer has won the game return his/her
     // evaluated score
-    if (score == 10)
-        return score;
+    if (puntaje == 10){
+        return puntaje;
+    }
  
     // If Minimizer has won the game return his/her
     // evaluated score
-    if (score == -10)
-        return score;
+    if (puntaje == -10){
+        return puntaje;
+    }
  
     // If there are no more moves and no winner then
     // it is a tie
-    if (isMovesLeft(board)==false)
+    if (Siquedanmovimientos(board)==false){
         return 0;
+    }
  
     // If this maximizer's move
-    if (isMax)
-    {
-        int best = -1000;
+    if (isMax){
+        int mejor = -1000; //best
  
         // Traverse all cells
-        for (int i = 0; i<3; i++)
-        {
-            for (int j = 0; j<3; j++)
-            {
+        for (i=0;i<3;i++){
+            for (j=0;j<3;j++){
                 // Check if cell is empty
-                if (board[i][j]=='_')
-                {
+                if (matriz[i][j]==' '){
                     // Make the move
-                    board[i][j] = player;
+                    matriz[i][j] = caracterus;
  
                     // Call minimax recursively and choose
                     // the maximum value
-                    best = max( best,
-                        minimax(board, depth+1, !isMax) );
+                    mejor = max( mejor,
+                        minimax(matriz, depth+1, !isMax) );
  
                     // Undo the move
-                    board[i][j] = '_';
+                    matriz[i][j] = ' ';
                 }
             }
         }
-        return best;
+        return mejor;
     }
  
     // If this minimizer's move
     else
     {
-        int best = 1000;
+        int mejor = 1000;
  
         // Traverse all cells
-        for (int i = 0; i<3; i++)
+        for (i = 0; i<3; i++)
         {
-            for (int j = 0; j<3; j++)
+            for (j = 0; j<3; j++)
             {
                 // Check if cell is empty
-                if (board[i][j]=='_')
+                if (matriz[i][j]==' ')
                 {
                     // Make the move
-                    board[i][j] = opponent;
+                    board[i][j] = oponente;
  
                     // Call minimax recursively and choose
                     // the minimum value
-                    best = min(best,
-                           minimax(board, depth+1, !isMax));
+                    mejor = min(mejor,
+                           minimax(matriz, depth+1, !isMax));
  
                     // Undo the move
-                    board[i][j] = '_';
+                    matriz[i][j] = ' ';
                 }
             }
         }
-        return best;
+        return mejor;
     }
 }
  
 // This will return the best possible move for the player
-Move findBestMove(char board[3][3])
-{
-    int bestVal = -1000;
-    Move bestMove;
-    bestMove.row = -1;
-    bestMove.col = -1;
+Move buscamejormovida(char matriz[3][3]){//findBestove
+    int mejorVal = -1000; int i,j;
+    Movida mejormovida;
+    mejormovida.fil = -1;
+    mejormovida.col = -1;
  
     // Traverse all cells, evalutae minimax function for
     // all empty cells. And return the cell with optimal
     // value.
-    for (int i = 0; i<3; i++)
-    {
-        for (int j = 0; j<3; j++)
-        {
+    for (i = 0; i<3; i++){
+        for (int j = 0; j<3; j++){
             // Check if celll is empty
-            if (board[i][j]=='_')
-            {
+            if (matriz[i][j]==' '){
                 // Make the move
-                board[i][j] = player;
+                matriz[i][j] = caracterus;
  
                 // compute evaluation function for this
                 // move.
-                int moveVal = minimax(board, 0, false);
+                int movidaVal = minimax(board, 0, false);
  
                 // Undo the move
-                board[i][j] = '_';
+                matriz[i][j] = ' ';
  
                 // If the value of the current move is
                 // more than the best value, then update
                 // best/
-                if (moveVal > bestVal)
+                if (movidaVal > mejorVal)
                 {
-                    bestMove.row = i;
-                    bestMove.col = j;
-                    bestVal = moveVal;
+                    mejormovida.fil = i;
+                    mejormovida.col = j;
+                    mejorVal = movidaVal;
                 }
             }
         }
     }
  
-    printf("The value of the best Move is : %d\n\n",
-            bestVal);
+    printf("The value of the best Move is : %d\n\n",mejorVal);
  
-    return bestMove;
+    return mejormovida;
 }
  
 // Driver code
